82f2569fdb1771fef085638383c9627f1ceddeab
Harist <ar.harist@yahoo.co.id>
2017-05-07 08:42:50 +0700

    modify application config file

--- src/config/cyom.php
+++ src/config/cyom.php
@@ -2,22 +2,22 @@
 
 return [
 
+    //application title / company name
     'company' => 'your company',
 
+    //year application publish
     'publish' => '2017',
 
+    //show / hide authorization menu and crud generator
+    'disable_generator' => false,
 
     //flag using custom template
     'custom_template' => false,
 
-    //custom template path for the generator.
-    
+    //custom template path for the generator.    
     'path' => base_path('resources/template/'),
 
-    /**
-     * Columns number to show in view's table.
-     */
+    //Columns number to show in view's table.
     'view_columns_number' => 3,
-
-    'disable_generator' => false,
+    
 ];
